5b07dc1acae209b926d77d448c16a5309be3be1f
C4rol-sudo <acam5@aluno.ifal.edu.br>
2026-05-09 15:22:21 -0300

    Dia 1

---
+++ lib/conquistas.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Conquistas extends StatefulWidget {
+  const Conquistas({super.key});
+
+  @override
+  State<Conquistas> createState() => _conquistas();
+}
+
+class _conquistas extends State<Conquistas> {
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold
+        (
+        appBar: AppBar(
+            backgroundColor: Color(0xFFD0FFEC)
+        ),
+
+      );
+  }
+}
---
+++ lib/perfil.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Perfil extends StatefulWidget {
+  const Perfil({super.key});
+
+  @override
+  State<Perfil> createState() => _perfil();
+}
+
+class _perfil extends State<Perfil> {
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold
+        (
+        appBar: AppBar(
+          backgroundColor: Color(0xFFE4D8FF)
+        ),
+
+      );
+  }
+}
