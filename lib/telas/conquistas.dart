4d6f9d6a148cbe7cb9187d1fc6e6453d1bdeb94e
Rebeca Batista <rbs37@aluno.ifal.edu.br>
2026-06-23 16:05:17 -0300

    Update conquistas.dart

--- lib/telas/conquistas.dart
+++ lib/telas/conquistas.dart
@@ -1,23 +1,67 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+class _ConquistasState extends State<Conquistas> {
+ final DbHelperconquistas _dbHelper = DbHelperconquistas();
+ 
+ List<Map<String, dynamic>> listaConquistas = [];
 
-class Conquistas extends StatefulWidget {
-  const Conquistas({super.key});
 
-  @override
-  State<Conquistas> createState() => _conquistas();
-}
+ @override
+ void initState(){
+   super.initState();
+   _carregarConquistas();
+ }
 
-class _conquistas extends State<Conquistas> {
-  @override
-  Widget build(BuildContext context) {
-    return
-      Scaffold
-        (
-        appBar: AppBar(
-            backgroundColor: Color(0xFFA7EACE)
-        ),
 
-      );
-  }
+ void _carregarConquistas() async{
+   final dados = await _dbHelper.buscarConquistas();
+   setState((){
+
+
+     listaConquistas = dados;
+   });
+ }
+
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+       appBar: AppBar(
+           backgroundColor: Color(0xFFA7EACE)
+       ),
+      backgroundColor: Color(0xFFD0FFEC),
+
+
+     body: ListView.builder(
+       itemCount: listaConquistas.length,
+       itemBuilder: (context, index){
+         final conquista = listaConquistas[index];
+
+
+         final bool isDesbloqueado = conquista['desbloqueado'] == 1;
+
+
+         return ListTile(
+             leading: Icon (isDesbloqueado ? Icons.emoji_events : Icons.lock,
+             color: isDesbloqueado ? Colors.amber : Colors.grey),
+
+
+         title: Text(
+           conquista ['titulo'],
+           style: const TextStyle(fontWeight: FontWeight.bold),
+         ),
+           subtitle: Text(conquista['descricao']),
+
+
+           onTap: () async{
+               await _dbHelper.atualizarConquista(conquista['id']);
+
+
+               _carregarConquistas();
+           },
+         );
+       },
+     ),
+
+
+     );
+ }
 }
